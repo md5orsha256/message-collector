29020b08b6fc8137b3bfff0a3ce3863ad5813b9d
Dmitry Stepanov <stpnv.dmtry@gmail.com>
2020-07-06 19:00:01 +0600

    Added getting database credentials from env variables

--- main/settings.py
+++ main/settings.py
@@ -14,7 +14,7 @@ import os
 
 from main.parameters import Environments
 
-ENVIRONMENT = os.environ.get('ENVIRONMENT', Environments.DEVELOPMENT.value)
+ENVIRONMENT = os.getenv('ENVIRONMENT', Environments.DEVELOPMENT.value)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = ENVIRONMENT == Environments.DEVELOPMENT.value
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     }
 }
 
